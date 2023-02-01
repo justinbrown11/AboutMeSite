da6e0d5f69cbeeda2eeeee7d9c7513de1b308bef
justinbrown11 <96081581+justinbrown11@users.noreply.github.com>
2023-01-31 17:13:41 -0700

    converted to mvc

---
+++ AboutMe/Controllers/MainController.cs
@@ -0,0 +1,30 @@
+﻿using AboutMe.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AboutMe.Controllers
+{
+    public class MainController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult GradeCalc()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult GradeCalc(GradeCalcModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ AboutMe/Models/Class.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace AboutMe.Models
+{
+    public class GradeCalcModel
+    {
+        [Required]
+        [Range(0.00, 100.00)]
+        public decimal assignmentScore { get; set; }
+
+        [Required]
+        [Range(0.00, 100.00)]
+        public decimal groupProjectScore { get; set; }
+
+        [Required]
+        [Range(0.00, 100.00)]
+        public decimal quizScore { get; set; }
+
+        [Required]
+        [Range(0.00, 100.00)]
+        public decimal midtermScore { get; set; }
+
+        [Required]
+        [Range(0.00, 100.00)]
+        public decimal finalScore { get; set; }
+
+        [Required]
+        [Range(0.00, 100.00)]
+        public decimal intexScore { get; set; }
+    }
+}
